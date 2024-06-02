df91d12c82724a884a0a565c7a3276797bd7ed46
duclinh <linh1862002@gmail.com>
2024-06-02 19:03:47 +0700

    2-6-2024 completed share on twitter

--- component/Chart/LineChart/Chart.jsx
+++ component/Chart/LineChart/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useRef} from 'react';
 import { useRouter } from 'next/router';
 import {LineChart,Line, AreaChart,Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import Style from './LineChart.module.css'
@@ -10,7 +10,6 @@ import { SiWebmoney } from "react-icons/si";
 import { FaShare } from "react-icons/fa";
 import { BiQrScan } from "react-icons/bi";
 import { MdChevronRight } from "react-icons/md";
-import { MdOutlineShowChart } from "react-icons/md";
 import { IoIosArrowDown } from "react-icons/io";
 import { FaChartSimple } from "react-icons/fa6";
 import {FiCopy} from 'react-icons/fi';
@@ -35,13 +34,21 @@ function CustomTooltip({ payload, label, active, currency = "usd" }) {
 }
 
 const ChartComponent = ({data}) => {
+  const formatPrice = (value) => {
+    return `$${value.toFixed(2)}`; // Thêm kí tự "$" trước giá trị và giới hạn số thập phân đến hai chữ số
+  };
   return( 
-      <AreaChart width={800} height={400} data={data}>
+      <AreaChart width={750} height={400} data={data}>
           <CartesianGrid stroke="#f5f5f5" 
           />
           <XAxis dataKey="date" interval="preserveStart" // sát bên trái 
+          axisLine={false} //ẩn trục
+          tickLine={false}
           />
-          <YAxis dataKey="prices" domain={["auto", "auto"]} orientation="right" label=""/>
+          <YAxis dataKey="prices" domain={["auto", "auto"]} orientation="right" label="" tick={{ fontSize: 12 }} tickFormatter={formatPrice}
+          axisLine={false}// ẩn trục 
+          tickLine={false}
+          />
           <Tooltip content={<CustomTooltip />} cursor={true} wrapperStyle={{outline: "none"}}/>
           <Legend />
           <Area type="monotone" dataKey="prices" stroke="#677de6" strokeWidth={2} dot={false} fill="#b2bef5"  curve="monotoneX"/>
@@ -59,7 +66,10 @@ const Chart = () => {
   const [open1year, setOpen1Year] = useState(false);
   const [openAll, setOpenAll] = useState(false);
   const [currentAccount, setCurrenAccount] = useState('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
+  const [addressMyWeb, setAddressMyWeb] = useState('https://www.youtube.com/watch?v=5vcB-EUwbJU');
   const [showCheck, setShowCheck] = useState(false);
+  const [openShare, setOpenShare] = useState(false);
+  const [hovering, setHovering] = useState(false);
   const onHandleADay = () => {
     setActiveDay(1);
     setOpen1day(true);
@@ -107,6 +117,7 @@ const Chart = () => {
     }
   }, [router.isReady, router.query]);
 
+  // get api
   useEffect(() => {
     const fetchHistoricalData = async (coinId) => {
       if (!coinId) return;
@@ -150,12 +161,42 @@ const Chart = () => {
     copyText.select();
     navigator.clipboard.writeText(copyText.value);
 };
-  const handleMouseDown = () => {
-    setShowCheck(true);
+const copyAddressShare = () => {
+  const copyText = document.getElementById("myWeb")
+  copyText.select();
+  navigator.clipboard.writeText(copyText.value);
+  
+};
+  const handleMouseDown = (event) => {
+    setShowCheck(true); 
     setTimeout(() => {
       setShowCheck(false);
     }, 1200);
   };
+
+  // share on twitter
+  const shareOnTwitter = () => {
+    const url = 'https://www.youtube.com/watch?v=n0WuQoexCjI&t=24s';
+    const text = `This is a post check out the historical data for ${coinId.toUpperCase()} love you!`;
+    const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${encodeURIComponent(text)}`;
+    window.open(twitterUrl, '_blank');
+  };
+  const handleOpenShare = () => {
+    setOpenShare(!openShare)
+  }
+  // close model
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.target.closest(`.${Style.chart_network_share}`) && !event.target.closest(`.${Style.chart_network}`)) {
+        setOpenShare(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
   return (
     <div className={Style.chart}>
       <div className={Style.chart_box}>
@@ -165,24 +206,24 @@ const Chart = () => {
         <div>{coinId.charAt(0).toUpperCase() + coinId.slice(1).toLowerCase()}</div>
         <div>&nbsp;
           <div className={StyleWallet.Model_box_head_address}> 
-                    <div onClick={() => copyAddress()}
-                      onMouseDown={handleMouseDown}
-                    >                 
-                    <input type="text" 
-                        value={currentAccount}
-                        id='myInput'
-                    />
-                    {currentAccount && currentAccount.length > 10
-                        ? currentAccount.slice(0, 7) + "..." + currentAccount.slice(-3)
-                        : currentAccount}
-                    &nbsp;&nbsp;
-                    {showCheck ? <FaRegCheckCircle className={StyleWallet.Model_box_head_address_check}/>
-                     : <FiCopy className={StyleWallet.Model_box_head_address_icon}
-                     /> 
-                     }
-                    </div>
+            <div onClick={() => copyAddress()}
+              onMouseDown={handleMouseDown}
+            >                 
+            <input type="text" 
+                value={currentAccount}
+                id='myInput'
+            />
+            {currentAccount && currentAccount.length > 10
+                ? currentAccount.slice(0, 7) + "..." + currentAccount.slice(-3)
+                : currentAccount}
+            &nbsp;&nbsp;
+            {showCheck ? <FaRegCheckCircle className={StyleWallet.Model_box_head_address_check}/>
+              : <FiCopy className={StyleWallet.Model_box_head_address_icon}
+              /> 
+              }
             </div>
           </div>
+        </div>
         </div>
       <div className={Style.chart_title}>
       <div className={Style.chart_title_left_left}>
@@ -190,10 +231,35 @@ const Chart = () => {
         <div>&nbsp;{coinId.toUpperCase()}</div>
       </div>
         <div className={Style.chart_title_right}>
-          <div className={Style.chart_network}><a href="https://etherscan.io/"><BiQrScan className={Style.chart_network_icon}/><span className={Style.tooltip}>Explore</span></a></div>
-          <div className={Style.chart_network}><a href="https://etherscan.io/"><SiWebmoney className={Style.chart_network_icon}/><span className={Style.tooltip}>Website</span></a></div>
-          <div className={Style.chart_network}><a href="https://etherscan.io/"><FaXTwitter className={Style.chart_network_icon}/><span className={Style.tooltip}>Twitter</span></a></div>
-          <div className={Style.chart_network}><a href="https://etherscan.io/"><FaShare className={Style.chart_network_icon}/><span className={Style.tooltip}>Share</span></a></div>
+          <div className={Style.chart_network}><a target='_blank' href="https://etherscan.io/"><BiQrScan className={Style.chart_network_icon}/><span className={Style.tooltip}>Explore</span></a></div>
+          <div className={Style.chart_network}><a target='_blank' href="https://etherscan.io/"><SiWebmoney className={Style.chart_network_icon}/><span className={Style.tooltip}>Website</span></a></div>
+          <div className={Style.chart_network}><a target='_blank' href="https://etherscan.io/"><FaXTwitter className={Style.chart_network_icon}/><span className={Style.tooltip}>Twitter</span></a></div>
+          <div className={Style.chart_network}
+          onClick={handleOpenShare}><FaShare className={Style.chart_network_icon}/>
+          {hovering && <span className={Style.tooltip}>Share</span>}
+            {
+              openShare && 
+              <div className={Style.chart_network_share} onClick={e => e.stopPropagation()}
+              onMouseEnter={() => setHovering(false)}
+              onMouseLeave={() => setHovering(true)}
+              >
+              <span onClick={() => copyAddressShare()}
+                  onMouseDown={handleMouseDown}>
+                <input type="text" 
+                    hidden
+                    value={addressMyWeb}
+                    id='myWeb'
+                />
+                {showCheck ? <FaRegCheckCircle className={StyleWallet.Model_box_head_address_check}/>
+                  : <FiCopy className={StyleWallet.Model_box_head_address_icon}
+                  /> 
+                }
+                  Copy link         
+              </span>
+              <span onClick={shareOnTwitter}><FaXTwitter/> Share to Twitter</span>
+            </div>
+            }
+          </div>
         </div>
       </div>
       <ChartComponent data = {data}/>
